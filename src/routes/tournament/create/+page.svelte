73d655e617bb59f5432986bb21b3f91390501162
Crenshinibon <dirk@accoma.de>
2026-05-17 17:29:35 +0200

    things

--- src/routes/tournament/create/+page.svelte
+++ src/routes/tournament/create/+page.svelte
@@ -10,7 +10,7 @@
 	let customPointsToWin = $state(21);
 	let customWinBy = $state(2);
 	let customDecidingPoints = $state(15);
-	let numRounds = $state(3);
+	let numRounds = $state(4);
 
 	const minPlayers = 8;
 	const maxPlayers = 64;
@@ -85,7 +85,7 @@
 	});
 
 	$effect(() => {
-		if (formatType === 'random-seed' && computedPlayerCount >= minPlayers) {
+		if (formatType === 'preseed' && computedPlayerCount >= minPlayers) {
 			numRounds = roundCounts();
 		}
 	});
